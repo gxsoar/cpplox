49ab3e09d4526d31a4ddb07f849e57369ef5814c
gxsoar <2278926493gx@gmail.com>
2024-04-02 16:06:09 +0800

    add enum class TokenType class Token

---
+++ include/token.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <string>
+#include <utility>
+
+namespace cpplox {
+
+enum class TokenType {
+  // Single-character tokens.
+  LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
+  COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
+
+  // One or two character tokens.
+  BANG, BANG_EQUAL,
+  EQUAL, EQUAL_EQUAL,
+  GREATER, GREATER_EQUAL,
+  LESS, LESS_EQUAL,
+
+  // Literals.
+  IDENTIFIER, STRING, NUMBER,
+
+  // Keywords.
+  AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+  PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+  TOKEN_EOF
+};
+
+class Token {
+public:
+  Token(TokenType token_type, std::string lexeme, int line) : 
+    token_type_(token_type), lexeme_(std::move(lexeme)), line_(line) {}
+  // TODO(gaoxiang): 
+  // auto ToString() -> std::string {
+    
+  // }
+private:
+  TokenType token_type_;
+  std::string lexeme_;
+  int line_;
+};
+
+}  // namespace cpplox
