b032a8ac526520676645089dbc20afbce2a3d80d
gxsoar <2278926493gx@gmail.com>
2024-04-02 16:38:52 +0800

    move class lox to lox.h

---
+++ src/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include "lox.h"
+
+
+auto main(int argc, const char *argv[]) -> int {
+  cpplox::Lox driver;
+  if (argc > 1) {
+    std::cout << "Usage: cpplox [script]\n";
+    return 0;
+  } 
+  if (argc == 1) {
+    driver.RunFile(argv[0]);
+  } else {
+    driver.RunPrompt();  
+  }
+  return 0;
+}
